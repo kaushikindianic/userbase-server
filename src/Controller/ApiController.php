84eeca96b1a3f61035cc3c7a329402968b53c53a
j.faassen <j.faassen@linkorb.com>
2016-02-15 15:27:51 +0100

    Support accounts/ api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -87,34 +87,115 @@ class ApiController
         }
         return $data;
     }
-    
-    public function userListAction(Application $app)
+
+
+    private function account2array(Application $app, $account, $details = false)
     {
+        if (!isset($app['userbase.partition'])) {
+            throw new RuntimeException("userbase.partition undefined");
+        }
+        $accountRepo = $app->getAccountRepository();
+        $userRepo = $app->getUserRepository();
+
+        $partition = strtolower($app['userbase.partition']);
+        $data = array();
+        $rolesData = array();
+        $data['href'] = $this->baseUrl . '/api/v1/accounts/' . $account->getName();
+        $data['name'] = $account->getName();
+        $data['type'] = $account->getAccountType();
+        
+        if ($details) {
+            $data['display_name'] = $account->getDisplayName();
+            $data['about'] = $account->getAbout();
+            $data['picture_url'] = $account->getPictureUrl();
+            $data['email'] = $account->getEmail();
+            $data['created_at'] = $account->getCreatedAt();
+            $data['deleted_at'] = $account->getDeletedAt();
+            
+            // GET USER ACCOUNTS //
+            $members = $accountRepo->getAccountMembers($account->getName());
+            
+            $data['members'] = array();
+            foreach ($members as $member) {
+                $memberData = array();
+                $memberData['user_name'] = $member['user_name'];
+                $memberData['is_owner'] = $member['is_owner'];
+                
+                $data['members'][] = $memberData;
+
+            }
+        }
+    
+        return $data;
+    }
+    
+    public function userIndexAction(Application $app, Request $request)
+    {
+        $details = false;
+        if ($request->query->has('details')) {
+            $details = true;
+        }
+        
         $this->baseUrl = $app['userbase.baseurl'];
         $repo = $app->getUserRepository();
         $users = $repo->getAll();
         $data = array();
         $items = array();
         foreach ($users as $user) {
-            $a = $this->user2array($app, $user);
+            $a = $this->user2array($app, $user, $details);
+            $items[] = $a;
+        }
+        $data['items'] = $items;
+        return new JsonResponse($data);
+    }
+
+    public function userViewAction(Application $app, $userName)
+    {
+        $this->baseUrl = $app['userbase.baseurl'];
+        $repo = $app->getUserRepository();
+        $user = $repo->getByName($userName);
+        if (!$user) {
+            return $this->getErrorResponse(404, "User not found");
+        }
+        $data = $this->user2array($app, $user, true);
+
+        return new JsonResponse($data);
+    }
+    
+    public function accountIndexAction(Application $app, Request $request)
+    {
+        $this->baseUrl = $app['userbase.baseurl'];
+        $details = false;
+        if ($request->query->has('details')) {
+            $details = true;
+        }
+        
+        $repo = $app->getAccountRepository();
+        $accounts = $repo->getAll();
+        $data = array();
+        $items = array();
+        foreach ($accounts as $account) {
+            $a = $this->account2array($app, $account, $details);
             $items[] = $a;
         }
         $data['items'] = $items;
         return new JsonResponse($data);
     }
     
-    public function userViewAction(Application $app, $username)
+    public function accountViewAction(Application $app, $accountName)
     {
         $this->baseUrl = $app['userbase.baseurl'];
-        $repo = $app->getUserRepository();
-        $user = $repo->getByName($username);
-        if (!$user) {
-            return $this->getErrorResponse(404, "User not found");
+        $repo = $app->getAccountRepository();
+        $account = $repo->getByName($accountName);
+        if (!$account) {
+            return $this->getErrorResponse(404, "Account not found");
         }
-        $data = $this->user2array($app,$user, true);
+        $data = $this->account2array($app, $account, true);
 
         return new JsonResponse($data);
     }
+    
+
     
     private function getErrorResponse($code, $message)
     {
