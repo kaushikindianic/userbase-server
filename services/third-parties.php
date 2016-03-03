f46b07754d23b0f718fd75612c444656425b8057
Hongliang <h.wang@linkorb.com>
2016-03-03 15:29:37 +0100

    minor fixes

--- services/third-parties.php
+++ services/third-parties.php
@@ -10,14 +10,14 @@
  */
 function getOAuth2Client(Array $config, $app)
 {
-    $redirectUri = $app['userbase.baseurl'] . $config['return_url'];
-    $scopes      = $config['scopes'];
+    $redirectUri = $app['userbase.baseurl'].(isset($config['return_url'])?$config['return_url']:'');
+    $scopes      = isset($config['scopes'])?$config['scopes']:null;
     $services    = array();
     if (isset($config['services']) && is_array($config['services'])) {
         foreach ($config['services'] as $service => $settings) {
             $settings = array_merge($settings, compact('redirectUri', 'scopes'));
-            $settings['redirectUri'] .= '/' . $service;
-            $class    = 'League\OAuth2\Client\Provider\\' . ucfirst($service);
+            $settings['redirectUri'] .= '/'.$service;
+            $class    = 'League\OAuth2\Client\Provider\\'.ucfirst($service);
             $services[$service] = new $class($settings);
         }
     }
