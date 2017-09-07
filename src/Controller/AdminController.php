818ba753235aa69bc2868b2c592a1361f16db228
kaushikindianic <kaushik@indianic.com>
2017-09-07 14:47:49 +0530

    Card #615: Userbase: random password generator api keys

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace UserBase\Server\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Exception;
+use Hackzilla\PasswordGenerator\Generator\RequirementPasswordGenerator;
+use Hackzilla\PasswordGenerator\RandomGenerator\Php7RandomGenerator;
 
 class AdminController
 {
-
     public function indexAction(Application $app, Request $request)
     {
         $data = array();
+
         return new Response($app['twig']->render('admin/index.html.twig', $data));
     }
 
@@ -19,10 +21,34 @@ class AdminController
     {
         $oEventRepo = $app->getEventRepository();
         $aEvents = $oEventRepo->getAll();
-        
+
         return new Response($app['twig']->render(
             'admin/event_index.html.twig',
-            array( 'aEvents' => $aEvents )
+            array('aEvents' => $aEvents)
+        ));
+    }
+
+    public function addApikeyAction(Application $app, Request $request)
+    {
+        $generator = new RequirementPasswordGenerator();
+
+        $generator
+          ->setLength(32)
+          ->setRandomGenerator(new Php7RandomGenerator())
+          ->setOptionValue(RequirementPasswordGenerator::OPTION_UPPER_CASE, true)
+          ->setOptionValue(RequirementPasswordGenerator::OPTION_LOWER_CASE, true)
+          ->setOptionValue(RequirementPasswordGenerator::OPTION_NUMBERS, true)
+          ->setOptionValue(RequirementPasswordGenerator::OPTION_SYMBOLS, false)
+          ->setMinimumCount(RequirementPasswordGenerator::OPTION_UPPER_CASE, 1)
+          ->setMinimumCount(RequirementPasswordGenerator::OPTION_LOWER_CASE, 1)
+          ->setMinimumCount(RequirementPasswordGenerator::OPTION_NUMBERS, 1)
+        ;
+
+        $password = $generator->generatePassword();
+
+        return new Response($app['twig']->render(
+            'admin/password_generator.html.twig',
+            array('password' => $password)
         ));
     }
 }
